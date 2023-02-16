0c06c235483ed3a84c33812b25c19a7717a7fbb3
Duck-Citizenf <slavabogdanov98@gmail.com>
2023-02-16 22:30:32 +0300

    да

--- src/content/page.ts
+++ src/content/page.ts
@@ -37,6 +37,36 @@ if (!window.shopperExtensionInstalled) {
             }
         }
     });
+    
+    const config = { childList: true, subtree: true };
+    const observer = new MutationObserver((mutationList, observer) => {
+        for (const mutation of mutationList) {
+            mutation.addedNodes.forEach((a) => {
+                calculate(a as Element);
+            })
+        }
+    })
+    observer.observe(document.querySelector('[data-test-id="virtuoso-item-list"]')!, config);
+    setTimeout(calculate, 1000);
+    function calculate(a: Document|Element){
+        const price = a.querySelectorAll<HTMLElement>('[data-zone-name="price"] > div > a > div > span > span:first-of-type');
+        const mass = a.querySelectorAll<HTMLElement>('[data-auto="product-title"]');
+        price.forEach((m,i) => {
+            let innerPrice = parseFloat(m.innerText.replace(/ /g,""));
+            let regular_mass = mass[i].title.match(/ ([\d.]+) (к?г)/);
+            if(regular_mass == null){
+                return;
+            }
+            let innerMass = parseFloat(regular_mass[1]);
+            if (regular_mass[2] == "кг"){
+                innerMass*=1000;
+            }
+            let result = innerPrice/(innerMass/100);
+            let result2 = result.toFixed(2).toString();
+
+            m.closest('a')!.append(result2+'₽ за 100г');
+        })
+    }
 }
 
 export {};
